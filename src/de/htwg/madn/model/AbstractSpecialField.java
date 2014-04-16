650c1235122a2a5e6f2b55e6960cfd0be1c5b093
sikessle <sikessle@192.168.1.5>
2014-04-16 16:52:54 +0200

    some more implementation of the transformer. still needs a lot of work.

--- src/de/htwg/madn/model/AbstractSpecialField.java
+++ src/de/htwg/madn/model/AbstractSpecialField.java
@@ -4,13 +4,12 @@ public abstract class AbstractSpecialField {
 
 	private Player owner;
 	private final Figure[] fields;
-	
-	
+
 	public AbstractSpecialField(final int fieldSize) {
 		owner = null;
 		fields = new Figure[fieldSize];
 	}
-	
+
 	public void setOwner(Player owner) {
 		if (owner == null) {
 			return;
@@ -21,22 +20,26 @@ public abstract class AbstractSpecialField {
 	public Player getOwner() {
 		return owner;
 	}
-	
+
 	public void removeFigure(final int fieldIndex) {
 		checkLegalFieldAccess(fieldIndex);
 		fields[fieldIndex].setCurrentFieldIndex(-1);
 		fields[fieldIndex] = null;
 	}
-	
+
 	public void setFigure(final int fieldIndex, final Figure figure) {
 		checkLegalFieldAccess(fieldIndex);
 		fields[fieldIndex] = figure;
 		figure.setCurrentFieldIndex(fieldIndex);
 	}
-	
+
 	public Figure getFigure(final int fieldIndex) {
 		checkLegalFieldAccess(fieldIndex);
 		return fields[fieldIndex];
+	}
+
+	public Figure[] getFields() {
+		return fields;
 	}
 
 	public boolean isEmpty() {
@@ -47,7 +50,7 @@ public abstract class AbstractSpecialField {
 		}
 		return true;
 	}
-	
+
 	private void checkLegalFieldAccess(final int fieldIndex) {
 		if (fieldIndex < 0 || fieldIndex > fields.length) {
 			throw new ArrayIndexOutOfBoundsException();
@@ -57,9 +60,9 @@ public abstract class AbstractSpecialField {
 	public int getSize() {
 		return fields.length;
 	}
-	
+
 	public abstract int getEntryIndex();
-	
+
 	public abstract int getExitIndex();
-	
+
 }
--- src/de/htwg/madn/model/hibernate/DomainObjectToPersistenceTransformer.java
+++ src/de/htwg/madn/model/hibernate/DomainObjectToPersistenceTransformer.java
@@ -1,16 +1,78 @@
 package de.htwg.madn.model.hibernate;
 
-import com.google.inject.Injector;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
+import de.htwg.madn.model.Figure;
+import de.htwg.madn.model.FinishField;
 import de.htwg.madn.model.GameId;
 import de.htwg.madn.model.IBoard;
+import de.htwg.madn.model.Player;
 
 public class DomainObjectToPersistenceTransformer {
 
 	public PersistenceBoard transform(IBoard board) {
 		PersistenceBoard persistenceBoard = new PersistenceBoard();
-		// persistenceBoard.setDice(board.getDice())..
+
+		persistenceBoard.setActivePlayer(transform(board.getActivePlayer()));
+
+		return persistenceBoard;
+	}
+
+	private PersistencePlayer transform(Player player) {
+		PersistencePlayer persistencePlayer = new PersistencePlayer();
+		persistencePlayer.setColor(player.getColor());
+		persistencePlayer.setFigures(transform(player.getFigures(),
+				persistencePlayer));
+		persistencePlayer.setFinishField(transform(player.getFinishField(),
+				persistencePlayer));
+		persistencePlayer.setHomeField(null); // TODO
+
 		return null;
+	}
+
+	private PersistenceFinishField transform(FinishField finishField,
+			PersistencePlayer owner) {
+		PersistenceFinishField result = new PersistenceFinishField();
+
+		result.setEntryIndex(finishField.getEntryIndex());
+		result.setFields(transform(finishField.getFields(), owner));
+		result.setOwner(owner);
+
+		return result;
+	}
+
+	private PersistenceFigure[] transform(Figure[] fields,
+			PersistencePlayer owner) {
+		return (PersistenceFigure[]) transform(Arrays.asList(fields), owner)
+				.toArray();
+	}
+
+	private List<PersistenceFigure> transform(List<Figure> figures,
+			PersistencePlayer owner) {
+		List<PersistenceFigure> result = new LinkedList<PersistenceFigure>();
+
+		for (Figure fig : figures) {
+			result.add(transform(fig, owner));
+		}
+
+		return result;
+	}
+
+	private PersistenceFigure transform(Figure fig, PersistencePlayer owner) {
+		PersistenceFigure persistenceFigure = new PersistenceFigure();
+
+		persistenceFigure.setAtFinishArea(fig.isAtFinishArea());
+		persistenceFigure.setAtHomeArea(fig.isAtHomeArea());
+		persistenceFigure.setCurrentFieldIndex(fig.getCurrentFieldIndex());
+		persistenceFigure.setFinished(fig.isFinished());
+		persistenceFigure.setLetter(fig.getLetter());
+		persistenceFigure.setOwner(owner);
+
+		return persistenceFigure;
 	}
 
 	public PersistenceGameId transform(GameId gameId) {
