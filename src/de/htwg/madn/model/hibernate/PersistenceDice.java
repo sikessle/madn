ca412d3021837a5eb407e1a05b7770b1b7e65229
sikessle <sikessle@192.168.1.5>
2014-04-16 14:53:20 +0200

    import cleanup

--- src/de/htwg/madn/model/hibernate/PersistenceDice.java
+++ src/de/htwg/madn/model/hibernate/PersistenceDice.java
@@ -7,8 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import de.htwg.madn.model.Player;
-
 @Entity
 public final class PersistenceDice implements Serializable {
 
