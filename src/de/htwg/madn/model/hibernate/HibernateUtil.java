63f04d4721ef730c3541f3d8eb3fafb18becbf6b
sikessle <sikessle@192.168.1.5>
2014-04-16 11:27:09 +0200

    started on adding persistence*classes

---
+++ src/de/htwg/madn/model/hibernate/HibernateUtil.java
@@ -0,0 +1,22 @@
+package de.htwg.madn.model.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+
+	static {
+		final AnnotationConfiguration cfg = new AnnotationConfiguration();
+		cfg.configure("/hibernate.cfg.xml");
+		sessionFactory = cfg.buildSessionFactory();
+	}
+
+	private HibernateUtil() {
+	}
+
+	public static SessionFactory getInstance() {
+		return sessionFactory;
+	}
+
+}
